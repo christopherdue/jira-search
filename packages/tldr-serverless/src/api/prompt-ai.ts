32d34d33eee7b62fde3f2bd5b0359fbb55163116
yoavle <yoavle@wix.com>
2023-12-21 16:46:13 +0200

    revert and add reponse headers

--- packages/tldr-serverless/src/api/prompt-ai.ts
+++ packages/tldr-serverless/src/api/prompt-ai.ts
@@ -1,8 +1,7 @@
-import { HttpError, type FunctionContext, type WebRequest } from "@wix/serverless-api";
+import { FullHttpResponse, HttpError, type FunctionContext, type WebRequest } from "@wix/serverless-api";
 import { APP_DEF_ID } from '../consts';
 import { generateTextByPrompt } from '@wix/ambassador-ds-wix-ai-gateway-v1-prompt/rpc';
-import { type GenerateTextByPromptResponse } from '@wix/ambassador-ds-wix-ai-gateway-v1-prompt/types';
-export const requestPrompt = async (ctx: FunctionContext, req: WebRequest): Promise<GenerateTextByPromptResponse> => {
+export const requestPrompt = async (ctx: FunctionContext, req: WebRequest): Promise<FullHttpResponse> => {
   const s2s = ctx.getConfig('s2s');
 
   const {text} = req.body;
@@ -13,11 +12,20 @@ export const requestPrompt = async (ctx: FunctionContext, req: WebRequest): Prom
 
   ctx.apiGatewayClient.signWith(APP_DEF_ID, s2s);
   const signedAspects = await ctx.apiGatewayClient.addServerSignature(ctx.aspects);
-  return await ctx.ambassador.request(generateTextByPrompt({
+  const promptResponse =  await ctx.ambassador.request(generateTextByPrompt({
     promptId: '2ffac328-6554-43c5-b606-784e5fcebd20',
     params: {
       text
     }
   }), signedAspects);
+
+return new FullHttpResponse({
+    status: 200,
+    body: promptResponse,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST',
+    },
+  });
 
 }
