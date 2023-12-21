30078ef56d090a73f51a9669b5b04af2b8947860
Amit Tamari <98021493+amittwix@users.noreply.github.com>
2023-12-21 16:38:52 +0200

    add preflight (#8)

--- packages/tldr-serverless/src/index.ts
+++ packages/tldr-serverless/src/index.ts
@@ -1,6 +1,21 @@
-import type { FunctionsBuilder } from '@wix/serverless-api';
-import {requestPrompt} from './api/prompt-ai'
-module.exports = function builder (builder: FunctionsBuilder) {
+import { FullHttpResponse, type FunctionsBuilder } from '@wix/serverless-api';
+import { requestPrompt } from './api/prompt-ai';
+
+module.exports = function builder(builder: FunctionsBuilder) {
   return builder
-  .addWebFunction('POST', '/prompt', requestPrompt)
+    .addWebFunction('POST', '/prompt', requestPrompt)
+    .addWebFunction(
+      'OPTIONS',
+      '/prompt',
+      async () =>
+        new FullHttpResponse({
+          status: 204,
+          headers: {
+            'Access-Control-Allow-Origin': 'https://wix.wixanswers.com',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+            'Access-Control-Expose-Headers': 'x-wix-request-id,x-seen-by',
+          },
+        }),
+    );
 };
